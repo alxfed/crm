7cb0f29a9afae5e67cd2245615cadff50907e20e
alxfed <alxfed@gmail.com>
2019-07-24 16:36:19 -0500

    contacts

--- pandas_contacts_csv.py
+++ pandas_contacts_csv.py
@@ -52,13 +52,67 @@ input_headers = ['Company ID', 'Last Modified Date', 'Lead Status', 'Total Reven
            'Associated Company ID', 'Associated Company'
 '''
 
-mapping = {}
+owner_to_email = {
+    'Melissa Conroy':	'mconroy@marfacabinets.com',
+    'Douglas Sumner':	'dsumner@marfacabinets.com',
+    'Ania Keller':	'imidari@marfacabinets.com',
+    'Bjorn Berkmortel':	'bberkmortel@marfacabinets.com',
+    'Alexander Doroshko':	'sashadoroshko@marfacabinets.com',
+    '': 'nobody@marfacabinets.com'
+}
 
+output = pd.DataFrame()
+
+# mandatory:
+output['Account Name/Account CRM ID'] = data['Associated Company ID']
+output['Company Name'] = data['Company Name']
+output['Contact CRM ID'] = data['Contact ID']
+output['Email Address'] = data['Email']
+output['Is Unsubscribed'] = data['Unsubscribed from all email']
+output['First Name'] = data['First Name']
+output['Last Name'] = data['Last Name']
+# optional about the person:
+output['Job function'] = data['Job function']
+output['Job Title'] = data['Job Title']
+output['Lifecycle Stage'] = data['Lifecycle Stage']
+output['Lead Status'] = data['Lead Status']
+output['Create Date'] = data['Create Date']
+output['Close Date'] = data['Close Date']
+output['Last Modified Date'] = data['Last Modified Date']
+output['Phone'] = data['Phone Number']
+output['Mobile Phone Number'] = data['Mobile Phone Number']
+output['Time of Last Visit'] = data['Time of Last Visit']
+output['Time of First Visit'] = data['Time of First Visit']
+output['Last Activity Date'] = data['Last Activity Date']
+output['First Conversion'] = data['First Conversion']
+output['First Conversion Date'] = data['First Conversion Date']
+#owner = row['Contact owner']
+output['Contact owner'] = 0
+# about the company:
+# output['Street Address'] = data['Street Address']
+# output['City'] = data['City']
+# output['State/Region'] = data['State/Region']
+# output['Postal Code'] = data['Postal Code']
+# output['Country'] = data['Country']
+output['Website'] = data['Website URL']
+output['Email Domain'] = data['Email Domain']
+output['Work email'] = data['Work email']
+# deals
+output['Associated Deals'] = data['Recent Deal Close Date']
+output['Recent Deal Close Date'] = data['Recent Deal Close Date']
+output['Recent Deal Amount'] = data['Recent Deal Amount']
+output['First Deal Created Date'] = data['First Deal Created Date']
+# money:
+output['Total Revenue'] = data['Total Revenue']
+
+
+
+'''
 output_headers = ['','']
 
 output = pd.DataFrame()
 
 email = contacts_data.loc[contacts_data['Contact CRM ID']==crm_id]['Email Address'].values[0]
+'''
 
-
-print(contacts_data_frame)
+print(output)
