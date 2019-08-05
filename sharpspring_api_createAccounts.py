da6651efb20be468b2300cb67bb57c2746e4c268
alxfed <alxfed@gmail.com>
2019-08-05 10:30:44 -0500

    all permits file

--- sharpspring_api_createAccounts.py
+++ sharpspring_api_createAccounts.py
@@ -60,19 +60,20 @@ for permit in permits:
                 Mobile_Phone = data['CONTRACTOR-GENERAL CONTRACTOR Phone Mobile'].values[0]
             else:
                 Mobile_Phone = ''
+            if not (Mobile_Phone == ''):
+                Phone = Mobile_Phone
+
             list_of_objects.append(
                     {'ownerID': '313468425',
                      'accountName': data['CONTRACTOR-GENERAL CONTRACTOR Name'].values[0],
                      'billingStreetAddress': data['CONTRACTOR-GENERAL CONTRACTOR Address'].values[0],
-                     'phone': Phone,
-                     'mobilePhoneNumber': Mobile_Phone,
+                     'phone': Phone
                      })
 
 data = {
         "method":"createAccounts",
         "params":
-            {"objects": list_of_objects
-             },
+            {"objects": list_of_objects},
         "id":uu
         }
 
@@ -85,3 +86,425 @@ e = r.json()
 #res = e['result']
 
 print('ok')
+
+'''
+{
+    "result": {
+        "creates": [
+            {
+                "success": true,
+                "error": null,
+                "id": 7745591298
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745592322
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745593346
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745594370
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745595394
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745596418
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745597442
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745598466
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745599490
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745600514
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745601538
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745602562
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745603586
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745604610
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745605634
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745606658
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745607682
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745608706
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745609730
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745610754
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745611778
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745612802
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745613826
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745614850
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745615874
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745616898
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745617922
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745618946
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745619970
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745620994
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745622018
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745623042
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745624066
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745625090
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745626114
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745627138
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745628162
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745629186
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745630210
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745631234
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745632258
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745633282
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745634306
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745635330
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745636354
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745637378
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745638402
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745639426
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745640450
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745641474
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745642498
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745643522
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745644546
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745645570
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745646594
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745647618
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745648642
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745649666
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745650690
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745651714
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745652738
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745653762
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745654786
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745655810
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745656834
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745657858
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745658882
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745659906
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745660930
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745661954
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745662978
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745664002
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745665026
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745666050
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745667074
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745668098
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745669122
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745670146
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745671170
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745672194
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745673218
+            },
+            {
+                "success": true,
+                "error": null,
+                "id": 7745674242
+            }
+        ]
+    },
+    "error": [],
+    "id": "2f6534cb-a9e7-40e8-9f77-d09dca8825cf",
+    "callCount": "4",
+    "queryLimit": "5000"
+'''
