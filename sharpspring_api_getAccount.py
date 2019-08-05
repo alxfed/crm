277f4c8cd85791a5b7baaafbbe60972ec55ae4a9
alxfed <alxfed@gmail.com>
2019-08-05 17:12:56 -0500

    all permits file

--- sharpspring_api_getAccount.py
+++ sharpspring_api_getAccount.py
@@ -8,14 +8,11 @@ import json
 ACCOUNT_ID = os.environ['ACCOUNT_ID']
 SECRET_KEY = os.environ['SECRET_KEY']
 uu = str(uuid.uuid4())
-
 data = {
-        "method":"getAccounts",
+        "method":"getAccount",
         "params":
                 {
-                        "where":{"ownerID":"313468425"},
-                        "limit":500,
-                        "offset":0
+                  "id":"7745634306"
                 },
         "id": uu
         }
@@ -24,22 +21,11 @@ json_data = json.dumps(data)
 api_access = "https://api.sharpspring.com/pubapi/v1/?accountID={}&secretKey={}".format(ACCOUNT_ID, SECRET_KEY)
 response = requests.post(url=api_access, json=data)
 
-dict_of_account_parameters = response.json()['result']['account'][0] # the first company out of 500
+dict_of_account_parameters = response.json()['result']['account'][0]
 keys = list(dict_of_account_parameters.keys())
 for key in keys:
         print(key, dict_of_account_parameters[key])
 
 print('ok')
 
-'''
-{"method":"getLeads",
- "params":{
- "where":{
-  "emailAddress":"scottshsp@gmail.com"
- },
- "limit":500,
- "offset":0
-},
-"id":123
-}
-'''
+# df.to_csv(path_or_buf='/media/alxfed/toca/aa-crm/500company_ids_plus_plus.csv', index=False)
