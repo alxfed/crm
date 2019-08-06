230278d2a5985e85051dc9fc6f3e0bcd45e6bfbe
alxfed <alxfed@gmail.com>
2019-08-06 08:09:42 -0500

    all permits file

--- sharpspring_api_getAccounts.py
+++ sharpspring_api_getAccounts.py
@@ -7,8 +7,43 @@ import json
 
 ACCOUNT_ID = os.environ['ACCOUNT_ID']
 SECRET_KEY = os.environ['SECRET_KEY']
-uu = str(uuid.uuid4())
 
+
+def RequestAccounts(**kwargs):
+        id      = kwargs.get('id', None)
+        ownerID = kwargs.get('ownerID', None)
+        if id or ownerID:
+                where = {}
+                if id:
+                        where.append('"where": {"id": str(id)}')
+
+        api_access = "https://api.sharpspring.com/pubapi/v1/?accountID={}&secretKey={}".format(ACCOUNT_ID, SECRET_KEY)
+        uid = str(uuid.uuid4())
+        data = {
+                "method": "getAccounts",
+                "params":
+                        {
+                                "where": {"ownerID": "313468425"},
+                                "limit": 500,
+                                "offset": 0
+                        },
+                "id": uid
+        }
+        json_data = json.dumps(data)
+        response = requests.post(url=api_access, json=json_data)
+
+        dict_of_account_parameters = response.json()['result']['account'][0]  # the first company out of 500
+        keys = list(dict_of_account_parameters.keys())
+        for key in keys:
+                print(key, dict_of_account_parameters[key])
+        account_data = 0
+        return account_data
+
+a = RequestAccounts(id = 313468425)
+
+print('ok')
+
+'''
 data = {
         "method":"getAccounts",
         "params":
@@ -19,27 +54,4 @@ data = {
                 },
         "id": uu
         }
-
-json_data = json.dumps(data)
-api_access = "https://api.sharpspring.com/pubapi/v1/?accountID={}&secretKey={}".format(ACCOUNT_ID, SECRET_KEY)
-response = requests.post(url=api_access, json=data)
-
-dict_of_account_parameters = response.json()['result']['account'][0] # the first company out of 500
-keys = list(dict_of_account_parameters.keys())
-for key in keys:
-        print(key, dict_of_account_parameters[key])
-
-print('ok')
-
-'''
-{"method":"getLeads",
- "params":{
- "where":{
-  "emailAddress":"scottshsp@gmail.com"
- },
- "limit":500,
- "offset":0
-},
-"id":123
-}
 '''
