b605791d632dfb0aa4f1c653080fa36b2641668b
alxfed <alxfed@gmail.com>
2019-08-10 07:30:49 -0500

    duplicate companies

--- csv_processing.py
+++ csv_processing.py
@@ -2,11 +2,16 @@ import csv
 from collections import namedtuple
 
 
+file_path = '/media/alxfed/toca/aa-crm/preparation/unknown_companies.csv'
 
-with open('stock.csv') as f:
-f_csv = csv.reader(f)
-headings = next(f_csv)
-Row = namedtuple('Row', headings)
-for r in f_csv:
-row = Row(*r)
-# Process row
+with open(file_path) as f:
+    f_csv = csv.reader(f)
+    headings = next(f_csv)
+    # replace spaces with underscores in headings
+    headings = [heading.replace(' ', '_') for heading in headings]
+    Row = namedtuple('Row', headings)
+    for r in f_csv:
+        row = Row(*r)
+        a = row.Name
+        b = row.Street_Address
+        pass
