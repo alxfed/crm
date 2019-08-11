a1052b9a44278f614839c6b2f7c6e6b70f603142
alxfed <alxfed@gmail.com>
2019-08-11 10:52:41 -0500

    topsbuh api

--- topsbuh/topsbuh_api_update_property_of_a_company.py
+++ topsbuh/topsbuh_api_update_property_of_a_company.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import csv
 import json
 
 
@@ -7,16 +8,17 @@ API_KEY = os.environ['API_KEY']
 ME_URL = 'https://api.hubapi.com/integrations/v1/me'
 COMPANIES_PROPERTIES_URL = 'https://api.hubapi.com/properties/v1/companies/properties'
 COMPANIES_URL = 'https://api.hubapi.com/companies/v2/companies/'
+update_path = '/media/alxfed/toca/aa-crm/preparation/companies_to_update.csv'
 
-data = {
-  "properties": [
-    {
-      "name": "phone",
-      "value": "+1 (773) 370-6857"
-    }
-  ]
-}
+data = {"properties": [
+                        {
+                          "name": "phone",
+                          "value": ""
+                        }
+                      ]
+        }
 
+data['properties']['value'] = '+1 (708) 335-2413'
 data_json = json.dumps(data)
 company_id = ''
 api_access = "{}{}?hapikey={}".format(COMPANIES_URL, company_id, API_KEY)
