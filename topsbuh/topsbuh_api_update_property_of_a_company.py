5b14e85c056f0283d2a9f879927e605a998eebd7
alxfed <alxfed@gmail.com>
2019-08-11 08:28:23 -0500

    topsbuh api

--- topsbuh/topsbuh_api_update_property_of_a_company.py
+++ topsbuh/topsbuh_api_update_property_of_a_company.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 
 API_KEY = os.environ['API_KEY']
@@ -7,9 +8,19 @@ ME_URL = 'https://api.hubapi.com/integrations/v1/me'
 COMPANIES_PROPERTIES_URL = 'https://api.hubapi.com/properties/v1/companies/properties'
 COMPANIES_URL = 'https://api.hubapi.com/companies/v2/companies/'
 
+data = {
+  "properties": [
+    {
+      "name": "phone",
+      "value": "+1 (773) 370-6857"
+    }
+  ]
+}
+
+data_json = json.dumps(data)
 company_id = ''
 api_access = "{}{}?hapikey={}".format(COMPANIES_URL, company_id, API_KEY)
-response = requests.get(url=api_access)
+response = requests.put(url=api_access, json=data_json)
 
 result = response.json()
 print(result)
