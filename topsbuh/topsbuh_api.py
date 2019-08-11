afbd4ce2f91ddcf05cfca8a1364b9b4230b91a34
alxfed <alxfed@gmail.com>
2019-08-11 07:48:28 -0500

    topsbuh api

---
+++ topsbuh/topsbuh_api.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+
+API_KEY = os.environ['API_KEY']
+COMPANIES_PROPERTIES_URL = 'https://api.hubapi.com/properties/v1/companies/properties'
+
+
+api_access = COMPANIES_PROPERTIES_URL + "?hapikey={}".format(API_KEY)
+result = requests.get(url=api_access)
+
+'''
+e = r.json()
+res = e['result']
+di = res['lead'][0]
+who = di['firstName']
+'''
+print(result)
