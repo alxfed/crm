52b7d0547717fcbf604569f982a95779e354161e
alxfed <alxfed@gmail.com>
2019-08-12 10:08:27 -0500

    topsbuh update through api

---
+++ topsbuh/topsbuh_api_deal_closure_update.py
@@ -0,0 +1,42 @@
+"""
+datetime to unix
+https://docs.python.org/3/library/datetime.html
+"""
+import requests
+import os
+import re
+import csv
+import json
+import time
+from collections import namedtuple
+from datetime import datetime
+
+API_KEY = os.environ['API_KEY']
+ME_URL = 'https://api.hubapi.com/integrations/v1/me'
+DEAL_URL = 'https://api.hubapi.com/deals/v1/deal/'
+update_path = '/media/alxfed/toca/aa-crm/preparation/hubspot-crm-exports-all-deals-2019-08-12.csv'
+
+# request data
+data = {"properties": [
+                        {
+                          "name": "closedate",
+                          "value": ""
+                        }
+                      ]
+        }
+
+with open(update_path) as f:
+    f_csv = csv.reader(f)
+    headers = next(f_csv)
+    tuple_headers = [re.sub('[^a-zA-Z_]', '_', h) for h in headers]
+    Row = namedtuple('Row', tuple_headers)
+    for r in f_csv:
+        row = Row(*r)
+        deal_id = row.Deal_ID
+        date = datetime.fromisoformat(row.Permit_Issue_Date)
+        data['properties'][0]['value'] = str(int(1000*date.timestamp()))
+        data_json = json.dumps(data)
+        api_access = "{}{}?hapikey={}".format(DEAL_URL, deal_id, API_KEY)
+        response = requests.put(url=api_access, json=data_json)
+
+print('ok')
