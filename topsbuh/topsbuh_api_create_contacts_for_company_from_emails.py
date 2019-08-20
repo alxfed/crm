9471ab6aff9b6aa96fbd347475f6922b361c809e
alxfed <alxfed@gmail.com>
2019-08-20 11:39:14 -0500

    create contacts by emails

--- topsbuh/topsbuh_api_create_contacts_for_company_from_emails.py
+++ topsbuh/topsbuh_api_create_contacts_for_company_from_emails.py
@@ -1,15 +1,11 @@
 import requests
 import os
-import re
 import csv
-import json
-import time
-import pandas as pd
 from collections import OrderedDict
+from random import randint
 
 
 API_KEY = os.environ['API_KEY']
-COMPANIES_PROPERTIES_URL = 'https://api.hubapi.com/properties/v1/companies/properties'
 CONTACT_CREATE_OR_UPDATE_URL = 'https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/'
 companies_created_with_emails_path = '/media/alxfed/toca/aa-crm/uploads/companies_created_with_emails.csv'
 contacts_created_path = '/media/alxfed/toca/aa-crm/uploads/contacts_created.csv'
@@ -24,7 +20,7 @@ input_columns = ['Name', 'Type', 'Phone Number', 'Phone Mobile',
                  'Facebook', 'Twitter', 'Google', 'Linkedin',
                  'companyId', 'emails']
 
-# request data
+# contact create request data
 data = {'properties':
     [
         {
@@ -33,7 +29,7 @@ data = {'properties':
         },
         {
           "property": "lastname",
-          "value": "Auto"
+          "value": ""
         },
         {
           "property": "company",
@@ -46,32 +42,37 @@ data = {'properties':
     ]
 }
 
+
 # output
 output_rows = []
-output_columns = ['Name', 'companyId', 'firstname', 'email', 'vid']
-output_row = OrderedDict
+output_columns = ['Name', 'companyId', 'firstname', 'lastname', 'email', 'vid']
+
 
 with open(companies_created_with_emails_path) as f:
     f_csv = csv.DictReader(f, restkey='Rest', restval='')
     for row in f_csv:
-        output_row.update({'Name': row['Name']})
-        output_row.update({'companyId': row['companyId']})
+        output_row = OrderedDict()
+        output_row['Name'] = row['Name']
+        output_row['companyId'] = row['companyId']
         list_of_emails = row['emails'].split(' ')
         for email in list_of_emails:
-            name, domain = email.split('@')
-            output_row.update({'firstname'})
+            name, _ = email.split('@')
+            lastname = 'Auto_' + str(randint(1, 999999))
+            output_row['firstname'] = name
+            output_row['lastname'] = lastname
+            output_row['email'] = email
             data['properties'][0]['value'] = name
+            data['properties'][1]['value'] = lastname
             data['properties'][2]['value'] = row['Name']
             req_url = '{}{}/'.format(CONTACT_CREATE_OR_UPDATE_URL, email)
             response = requests.request("POST", url=req_url, json=data,
                                         headers=headers, params=querystring)
-        if response.status_code == 200:
-            output_row.update({'companyId': response.json()['companyId']})
-            output_rows.append(row)
-            print('ok')
-        else:
-            print('not ok! ', response.status_code)
-
+            if response.status_code == 200:
+                output_row['vid'] = response.json()['vid']
+            else:
+                output_row['vid'] = ''
+            output_rows.append(output_row)
+            print(output_row, response.status_code)
 
 with open(contacts_created_path,'w') as f:
     f_csv = csv.DictWriter(f, output_columns)
