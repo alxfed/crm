19f205fdcf7ae3000b8ea12a62df6c93febe1562
alxfed <alxfed@gmail.com>
2019-09-10 11:13:05 -0500

    logged API calls

--- ymehcla/insert_into_log.py
+++ ymehcla/insert_into_log.py
@@ -3,6 +3,7 @@ log file in a database
 """
 import sqlite3
 
+
 def conn_and_make_cursor(abs_path):
     """
     Creates connection and makes a cursor. Returns both of them.
@@ -15,12 +16,28 @@ def conn_and_make_cursor(abs_path):
 
 
 def create_table_if_not_exists(curs, table_name, schema):
+    """
+    Creates a log table if it doesn't exist with the schema
+    and name provided in a function call
+    :param curs:
+    :param table_name:
+    :param schema:
+    :return:
+    """
     par = (table_name, schema)
     curs.execute("create table if not exists ? ?", par)
     return
 
 
 def add_line(conn, curs, table_name, line):
+    """
+    Adds a line to the log table and commits it
+    :param conn:
+    :param curs:
+    :param table_name:
+    :param line:
+    :return:
+    """
     par = (table_name, line)
     curs.execute("insert into ? values ?", par)
     conn.commit()
@@ -29,13 +46,14 @@ def add_line(conn, curs, table_name, line):
 
 def main():
     """
-
-    :return:
+    Opens the log database and commits log into it line by line after every
+    action performed within this function.
+    :return: return code
     """
     DB_PATH = '/media/alxfed/toca/dbase/logbase.sqlite'
     SCHE_MA = '(date text, trans text, symbol text, qty text, price text)'
     TBL_NAM = 'log_table_today'
-    print('Starting main ')
+    print('starting the main ')
     try:
         co, cu = conn_and_make_cursor(DB_PATH)
     except sqlite3.Error as er:
@@ -45,8 +63,11 @@ def main():
         create_table_if_not_exists(cu, TBL_NAM, SCHE_MA)
     except sqlite3.Error as er:
         print('sqlite3 Error ', er.args[0])
+        exit(247)
+    # main action
 
     co.close()
+    return
 
 
 if __name__ == '__main__':
