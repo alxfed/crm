9b41dd843b4c9e23f47fa6d83af67b27b66ca703
alxfed <alxfed@gmail.com>
2019-09-10 11:03:02 -0500

    all properties read

---
+++ ymehcla/experiment.py
@@ -0,0 +1,5 @@
+a = ("dist", "2", "time of the year")
+
+b = str(a)
+
+print('ok')
--- ymehcla/insert_into_log.py
+++ ymehcla/insert_into_log.py
@@ -1,35 +1,54 @@
+"""
+log file in a database
+"""
 import sqlite3
 
-
-db_path = '/media/alxfed/toca/dbase/logbase.sqlite'
-sche_ma = '(date text, trans text, symbol text, qty text, price text)'
-tbl_name = 'log_table_today'
-
-def ConnMakeCursor(abs_path):
-    conn = sqlite3.connect(abs_path, isolation_level=None)
+def conn_and_make_cursor(abs_path):
+    """
+    Creates connection and makes a cursor. Returns both of them.
+    :param abs_path: absolute path to the existing log database
+    :return: connection, cursor
+    """
+    conn = sqlite3.connect(abs_path) # , isolation_level=None) for working without commit
     curs = conn.cursor()
     return conn, curs
 
 
-def CreateTableIfNotExists(curs, table_name, schema):
+def create_table_if_not_exists(curs, table_name, schema):
     par = (table_name, schema)
     curs.execute("create table if not exists ? ?", par)
+    return
 
 
-def AddLine(curs, table_name, line):
+def add_line(conn, curs, table_name, line):
     par = (table_name, line)
     curs.execute("insert into ? values ?", par)
+    conn.commit()
+    return
 
 
-co, cu = ConnMakeCursor(db_path)
-try:
-    CreateTableIfNotExists(cu, tbl_name, sche_ma)
-except sqlite3.Error:
-    print('sqlite3 Error')
-li = ()
+def main():
+    """
+
+    :return:
+    """
+    DB_PATH = '/media/alxfed/toca/dbase/logbase.sqlite'
+    SCHE_MA = '(date text, trans text, symbol text, qty text, price text)'
+    TBL_NAM = 'log_table_today'
+    print('Starting main ')
+    try:
+        co, cu = conn_and_make_cursor(DB_PATH)
+    except sqlite3.Error as er:
+        print('sqlite3 Error ', er.args[0])
+        exit(246)
+    try:
+        create_table_if_not_exists(cu, TBL_NAM, SCHE_MA)
+    except sqlite3.Error as er:
+        print('sqlite3 Error ', er.args[0])
+
+    co.close()
 
 
-
-
-co.close()
-print('ok')
+if __name__ == '__main__':
+    main()
+    print('main - done')
