5fb39dbb9b6be930ad01e8ce85b222433017a256
alxfed <alxfed@gmail.com>
2019-09-10 14:12:35 -0500

    logged API calls

---
+++ ymehcla/next.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""...
+"""
+import sqlite3
+
+
+def main():
+    """
+    :return: 
+    """
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
