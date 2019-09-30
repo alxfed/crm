64215cefafc4f359ba2f5cd523c884a9ca6874d0
alxfed <alxfed@gmail.com>
2019-09-30 06:54:54 -0500

    search by domain

--- topsbuh/topsbuh_api_search_companies.py
+++ topsbuh/topsbuh_api_search_companies.py
@@ -3,12 +3,19 @@
 from hubspot.companies import search_for_company_by_domain
 
 
-domain = 'ethanalleninc.com'
-paramlist = ['domain', 'createdate', 'name', 'hs_lastmodifieddate']
+def main():
+    domain = 'ethanalleninc.com'
+    paramlist = ['domain', 'createdate', 'name', 'hs_lastmodifieddate']
 
-response = search_for_company_by_domain(domain, paramlist)
-if response:
-    print(response)
-else:
-    print('nothing')
-print('\n\nok')
+    response = search_for_company_by_domain(domain, paramlist)
+    if response:
+        print(response)
+    else:
+        print('nothing')
+    print('\n\nok\n\n')
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
