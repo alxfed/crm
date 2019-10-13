cb49b31bb16567bb6fa3454765d5e45219af8286
alxfed <alxfed@gmail.com>
2019-10-13 16:31:58 -0500

    associations of companies

--- topsbuh/topsbuh_api_get_associations_of_companies.py
+++ topsbuh/topsbuh_api_get_associations_of_companies.py
@@ -23,41 +23,49 @@ def make_parameters_string(offset, limit):
 
 # output
 output_rows = []
-output_columns = ['companyId', 'is_contact',
-                  'firstname', 'lastname', 'email', 'email_two',
-                  'jobtitle', 'company', 'phone', 'mobilephone',
-                  'city','zip','state', 'hs_lead_status']
+output_columns = ['companyId', 'associations']
 
 # prepare for the pagination
-has_more = True
 offset = 0
 limit = 100           # max 100
 
-while has_more:
-    api_url = '{}{}?{}'.format(ASSOCIATIONS_URL, ASSOCIATIONS_TYPE,
-                               make_parameters_string(offset, limit))
-    response = requests.request("GET", url=api_url, headers=headers)
-    if response.status_code == 200:
-        res         = response.json()
-        has_more    = res['hasMore']
-        vidOffset   = res['offset']
-        associations    = res['results']
-        '''
-        for association in associations:
-            row = {}
-            row.update({"vid": contact["vid"],
-                        "is_contact": contact["is-contact"]})
-            co_properties = contact['properties']
-            for co_property in co_properties:
-                if co_property not in output_columns:
-                    output_columns.append(co_property)
-                    print('Adding a property to colunms list: ', co_property)
-                row.update({co_property: co_properties[co_property]['value']})
-            output_rows.append(row)
-        print('vidOffset: ', vidOffset)
-        '''
-    else:
-        print('Error: ', response.status_code)
+with open(companies_path) as f:
+    f_csv = csv.DictReader(f, restkey='Rest', restval='')
+    for row in f_csv:
+        has_more = True
+        while has_more:
+            api_url = '{}{}{}?{}'.format(ASSOCIATIONS_URL,
+                                         row['companyId'],
+                                         ASSOCIATIONS_TYPE,
+                                         make_parameters_string(offset, limit))
+            response = requests.request("GET", url=api_url, headers=headers)
+            if response.status_code == 200:
+                res         = response.json()
+                has_more    = res['hasMore']
+                offset   = res['offset']
+                associations = res['results']
+                if associations:
+                    output_row = dict()
+                    output_row.update({'companyId': row['companyId'],
+                                       'associations': associations})
+                    output_rows.append(output_row)
+                    print('CompanyId:', row['companyId'], associations)
+                '''
+                for association in associations:
+                    row = {}
+                    row.update({"vid": contact["vid"],
+                                "is_contact": contact["is-contact"]})
+                    co_properties = contact['properties']
+                    for co_property in co_properties:
+                        if co_property not in output_columns:
+                            output_columns.append(co_property)
+                            print('Adding a property to colunms list: ', co_property)
+                        row.update({co_property: co_properties[co_property]['value']})
+                    output_rows.append(row)
+                print('vidOffset: ', vidOffset)
+                '''
+            else:
+                print('Error: ', response.status_code)
 
 all_contacts = DataFrame.from_records(data=output_rows, columns=output_columns)
 with open(downloaded_associations_path,'w') as f:
