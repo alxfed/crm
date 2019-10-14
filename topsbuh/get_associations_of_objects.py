fa8967674b94fb9ab07a2f04247817268e41a5a3
alxfed <alxfed@gmail.com>
2019-10-14 15:49:07 -0500

    hubspot module

--- topsbuh/get_associations_of_objects.py
+++ topsbuh/get_associations_of_objects.py
@@ -7,7 +7,7 @@ import csv
 
 def main():
     OBJECTID_FILE_PATH = '/media/alxfed/toca/aa-crm/enrich/companies_downloaded.csv'
-    ASSOCIATIONS_FILE_PATH = '/media/alxfed/toca/aa-crm/enrich/associations_downloaded.csv'
+    ASSOCIATIONS_FILE_PATH = '/media/alxfed/toca/aa-crm/enrich/both_company_associations_downloaded.csv'
     association_types = {'Contact to company': 1, 'Company to contact': 2,
                          'Deal to contact': 3, 'Contact to deal': 4,
                          'Deal to company': 5, 'Company to deal': 6,
@@ -22,18 +22,23 @@ def main():
         f_csv = csv.DictReader(f, restkey='Rest', restval='')
         for row in f_csv:
             output_row = {}
-            associations = ''
+            ass_one = ''
+            assoc = hubspot.associations.get_associations_of_object(row['companyId'], 1)
+            if assoc:
+                ass_one = " ".join(['%s' % n for n in assoc])
+            output_row.update({'companyId': row['companyId'],
+                               'Contact to company': ass_one})
+            ass_two = ''
             assoc = hubspot.associations.get_associations_of_object(row['companyId'], 2)
             if assoc:
-                associations = ' '.join(assoc)
-            output_row.update({'companyId': row['companyId'],
-                               'associations': associations})
+                ass_two = " ".join(['%s' % n for n in assoc])
+            output_row.update({'Company to contact': ass_two})
             output_rows.append(output_row)
 
     with open(ASSOCIATIONS_FILE_PATH, 'w') as f:
         f_csv = csv.DictWriter(f, fieldnames=output_columns)
         f_csv.writeheader()
-        f_csv.writerows(associations)
+        f_csv.writerows(output_rows)
     return
 
 
