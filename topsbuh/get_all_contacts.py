28cf9a7ea2439fd321da093cb07040cffb1fbb66
alxfed <alxfed@gmail.com>
2019-10-14 08:59:18 -0500

    hubspot module

--- topsbuh/get_all_contacts.py
+++ topsbuh/get_all_contacts.py
@@ -2,6 +2,7 @@
 """...
 """
 import hubspot
+import csv
 
 
 def main():
@@ -10,6 +11,10 @@ def main():
                   'jobtitle', 'company', 'phone', 'mobilephone',
                   'city', 'zip', 'state', 'hs_lead_status']
     all_contacts_cdr, all_columns = hubspot.contacts.get_all_contacts(request_params)
+    with open(DOWLOADED_CONTACTS_FILE_PATH, 'w') as f:
+        f_csv = csv.DictWriter(f, all_columns)
+        f_csv.writeheader()
+        f_csv.writerows(all_contacts_cdr)
     return
 
 
